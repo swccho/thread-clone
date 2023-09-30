44ddb7f9a1f995035872a9fc9a0f0192f55b76bf
Ali Haider Swccho <swccho263@gmail.com>
2023-09-30 22:43:57 +0600

    [master]-post page done

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,72 @@
 <template>
   <MainLayout>
-    Home
+    <div id="IndexPage" class="w-full overflow-auto">
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+        <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+          <div v-if="isPosts"  v-for="post in posts" :key="post">
+            <Post :post="post" @isDeleted="posts = []" />
+          </div>
+        </div>
+      </div>
+    </div>
   </MainLayout>
 </template>
 
 <script setup>
 import MainLayout from "~/layouts/MainLayout.vue";
 
+import {useUserStore} from "~/stores/user.js";
+const userStore = useUserStore()
+// const user = useSupabaseUser()
+
+let posts = ref([])
+let isPosts = ref(true)
+let isLoading = ref(false)
+
+onBeforeMount(() => {
+  posts.value = [
+    {
+      name:'John Weeks Dev',
+      image:'https://placehold.co/100',
+      text:'This is the title',
+      picture:'https://placehold.co/500',
+    },
+    {
+      name:'John Weeks Dev',
+      image:'https://placehold.co/100',
+      text:'This is the title',
+      picture:'https://placehold.co/500',
+    },
+    {
+      name:'John Weeks Dev',
+      image:'https://placehold.co/100',
+      text:'This is the title',
+      picture:'https://placehold.co/500',
+    },
+    {
+      name:'John Weeks Dev',
+      image:'https://placehold.co/100',
+      text:'This is the title',
+      picture:'https://placehold.co/500',
+    },
+    {
+      name:'John Weeks Dev',
+      image:'https://placehold.co/100',
+      text:'This is the title',
+      picture:'https://placehold.co/500',
+    },
+    {
+      name:'John Weeks Dev',
+      image:'https://placehold.co/100',
+      text:'This is the title',
+      picture:'https://placehold.co/500',
+    },
+    {
+      name:'John Weeks Dev',
+      image:'https://placehold.co/100',
+      text:'This is the title',
+      picture:'https://placehold.co/500',
+    },
+  ]
+})
 </script>
