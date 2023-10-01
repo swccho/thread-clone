3948868d3f8f93f7e7b3334ac552e3bc57a67d6f
Ali Haider Swccho <swccho263@gmail.com>
2023-10-01 10:22:27 +0600

    [master] working on auth page

---
+++ pages/auth.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Auth
+  </div>
+</template>
+
+<script setup>
+    // const client = useSupabaseClient()
+    // const user = useSupabaseUser()
+
+    // watchEffect(() => {
+    //   if(user.value){
+    //     return navigateTo('/')
+    //   }
+    // })
+
+    // const login = async (prov) => {
+    //   const {data, error} = await client.auth.signInWithAuth({
+    //     provider:prov,
+    //     redirectTo:window.location.origin
+    //   })
+    //
+    //   if (error) console.log(error)
+    // }
+</script>
